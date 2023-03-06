2e965ca678ad29ab835a0cced5e304509a2efedc
Szydlooo <filis.sadkowski@gmail.com>
2023-03-06 15:24:05 +0100

    updejt

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
+    public GameObject bulletPreFab;
+    public float bulletSpeed = 20;
+    public float playerSpeed = 2;
     Vector2 movementVector;
-   
+    Transform BulletSpawn;
     void Start()
     {
         movementVector = Vector2.zero;
+        BulletSpawn = transform.Find("BulletSpawn");
     }
 
     void Update()
     {
         transform.Rotate(Vector3.up * movementVector.x);
         transform.Translate(Vector3.forward * movementVector.y * Time.deltaTime);
+            
     }
    void OnMove(InputValue inputValue)
     {
         movementVector = inputValue.Get<Vector2>();
-        Debug.Log(movementVector.ToString());
+        //Debug.Log(movementVector.ToString());
+
+    }
+    void OnFire()
+    {
+      GameObject bullet = Instantiate(bulletPreFab, BulletSpawn);
+        bullet.transform.parent = null;
+        bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * bulletSpeed, ForceMode.VelocityChange);
+        Destroy(bullet, 1);
     }
 }
