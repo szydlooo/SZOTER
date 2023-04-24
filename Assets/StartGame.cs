d6903559130faa8eceb47361d1955c1d3e07c833
Szydlooo <filis.sadkowski@gmail.com>
2023-04-24 15:20:56 +0200

    main menu

---
+++ Assets/StartGame.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public void GameStart()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
